cb18ee7832c9b6f9a68132a68dc8fdb9af1ed7ea
Paddyk45 <patrickk0@tutanota.com>
2024-04-17 16:38:31 +0200

    fix missing s when diff is 1

--- src/storage.rs
+++ src/storage.rs
@@ -23,30 +23,48 @@ impl Article {
         }
         
         if diff < 60 {
+            if diff == 1 {
+                return format!("{diff} second ago")
+            }
             return format!("{diff} seconds ago")
         }
         
         diff /= 60;
         if diff < 60 {
+            if diff == 1 {
+                return format!("{diff} minute ago")
+            }
             return format!("{diff} minutes ago")
         }
         
         diff /= 60;
         if diff < 24 {
+            if diff == 1 {
+                return format!("{diff} hour ago")
+            }
             return format!("{diff} hours ago")
         }
         
         diff /= 24;
         if diff < 30 {
+            if diff == 1 {
+                return format!("{diff} day ago")
+            }
             return format!("{diff} days ago")
         }
         
         diff /= 30;
         if diff < 12 {
+            if diff == 1 {
+                return format!("{diff} month ago")
+            }
             return format!("{diff} months ago")
         }
         
         diff /= 12;
+        if diff == 1 {
+            return format!("{diff} year ago")
+        }
         format!("{diff} years ago")
     }
 }
