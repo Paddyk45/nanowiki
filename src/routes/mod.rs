e91b872b2ab78105f8194068e5a274a541410865
Patrick <73178245+Paddyk45@users.noreply.github.com>
2024-05-09 18:21:58 +0200

    Update mod.rs

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,11 +1,11 @@
 mod article;
-mod detete;
+mod delete;
 mod edit;
 mod index;
 mod update;
 
 pub use article::article_route;
-pub use detete::delete_route;
+pub use delete::delete_route;
 pub use edit::edit_route;
 pub use index::index_route;
 pub use update::update_route;
